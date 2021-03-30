32c0c2862e88a6da1bc7e8f9c4d022ad25becfe0
Russell Archer <russell.archer@mac.com>
2021-03-30 23:14:42 +0100

    Initial Commit

---
+++ SwiftUINavDemo/SwiftUINavDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavDemoApp.swift
+//  SwiftUINavDemo
+//
+//  Created by Russell Archer on 30/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
